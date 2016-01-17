faa03b0c9e0a5da08a305c80dbdc71de1869d103
Alex Bafaloukos <alexbafaloukos@gmail.com>
2016-01-16 22:46:10 -0500

    InputManager that can read movement patterns, beginning of UI

---
+++ Project1/Assets/Scripts/InputManager.cs
@@ -0,0 +1,118 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class InputManager : MonoBehaviour {
+
+    public delegate void StartInputSeriesAction();
+    public delegate void StartInputSeriesWindowAction();
+    public delegate void EndInputSeriesWindowAction();
+    public delegate void EndInputSeriesAction();
+    public enum InputType { NONE, PRESS, DOUBLETAP, HOLD, RELEASE };
+
+    public static InputManager Instance = null;
+    public static event StartInputSeriesAction OnStartInputSeries;
+    public static event StartInputSeriesWindowAction OnStartInputSeriesWindow;
+    public static event EndInputSeriesWindowAction OnEndInputSeriesWindow;
+    public static event EndInputSeriesAction OnEndInputSeries;
+
+    string InputButton = "OneButton";
+    public float InputWindowLength = 1.0f;
+    public List<InputType> InputString = new List<InputType>();
+    public bool IsGettingInputString = false;
+    public int CurrentInputStringLength = 0;
+
+    
+    void Awake() {
+        if (Instance == null) {
+            Instance = this;
+        }
+        else {
+            Destroy(this);
+        }
+    } 
+
+	void Start () {
+        OnStartInputSeries += TestNewInputSeries;
+        OnStartInputSeriesWindow += TestNewInputWindow;
+        OnEndInputSeriesWindow += TestEndInputWindow;
+        OnEndInputSeries += TestEndInputSeries;
+	}
+	
+	void Update () {
+	    if (Input.GetKeyUp(KeyCode.RightShift) && !IsGettingInputString) {
+            StartCoroutine(GenerateNewInputSeries(5));
+        }
+	}
+
+    public IEnumerator GenerateNewInputSeries(int length) {
+        IsGettingInputString = true;
+        CurrentInputStringLength = length;
+        InputString.Clear();
+
+        if (OnStartInputSeries != null) OnStartInputSeries();
+
+        int currentWindow = 0;
+        bool currentlyHeld = false;
+        while (currentWindow < length) {
+            if (OnStartInputSeriesWindow != null) OnStartInputSeriesWindow();
+
+            float windowTime = 0;
+            int timesPressed = 0;
+            int timesReleased = 0;
+            bool startHeld = currentlyHeld;
+            while (windowTime < InputWindowLength) {
+                if (Input.GetAxisRaw(InputButton) > float.Epsilon && !currentlyHeld) {
+                    currentlyHeld = true;
+                    timesPressed++;
+                }
+                else if (Input.GetAxisRaw(InputButton) <= float.Epsilon && currentlyHeld) {
+                    currentlyHeld = false;
+                    timesReleased++;
+                }
+
+                windowTime += Time.deltaTime;
+                yield return null;
+            }
+
+            
+            if (startHeld && timesReleased > 0) {
+                InputString.Add(InputType.RELEASE);
+            }
+            else if (timesPressed == 1 && timesReleased == 1) {
+                InputString.Add(InputType.PRESS);
+            }
+            else if (timesPressed > 1 && timesReleased > 1) {
+                InputString.Add(InputType.DOUBLETAP);
+            }
+            else if (timesPressed > timesReleased) {
+                InputString.Add(InputType.HOLD);
+            }
+            else {
+                InputString.Add(InputType.NONE);
+            }
+
+            if (OnEndInputSeriesWindow != null) OnEndInputSeriesWindow();
+
+            currentWindow++;
+        }
+
+        if (OnEndInputSeries != null) OnEndInputSeries();
+
+        IsGettingInputString = false;
+        CurrentInputStringLength = 0;
+    }
+
+    public void TestNewInputSeries() {
+        Debug.Log("New input series");
+    }
+    public void TestNewInputWindow() {
+        Debug.Log("New input window");
+    }
+    public void TestEndInputWindow() {
+        Debug.Log("End input window");
+    }
+    public void TestEndInputSeries() {
+        Debug.Log("End input series");
+    }
+}
---
+++ Project1/Assets/Scripts/InputSeriesUI.cs
@@ -0,0 +1,63 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+using UnityEngine.UI;
+
+public class InputSeriesUI : MonoBehaviour {
+
+    public GameObject Timeline = null;
+    List<Image> _windowMarkers = new List<Image>();
+    Image _progressBar = null;
+
+	void Start () {
+        InputManager.OnStartInputSeries += OnInputSeriesStart;
+        InputManager.OnStartInputSeriesWindow += OnInputWindowStart;
+        InputManager.OnEndInputSeriesWindow += OnInputWindowEnd;
+        InputManager.OnEndInputSeries += OnInputSeriesEnd;
+        	
+	}
+	
+	void Update () {
+	    if (_progressBar && _progressBar.rectTransform.anchorMax.x < 1) {
+            float adjust = (Time.deltaTime / InputManager.Instance.InputWindowLength) / InputManager.Instance.CurrentInputStringLength;
+            _progressBar.rectTransform.anchorMax = new Vector2(_progressBar.rectTransform.anchorMax.x + adjust, _progressBar.rectTransform.anchorMax.y);
+            if (_progressBar.rectTransform.anchorMax.x > 1) _progressBar.rectTransform.anchorMax = new Vector2(1, _progressBar.rectTransform.anchorMax.y);
+        }
+	}
+
+    void OnInputSeriesStart() {
+        for (int i=0; i<=InputManager.Instance.CurrentInputStringLength; i++) {
+            GameObject newWindowMarker = new GameObject("WindowMarker " + i);
+            newWindowMarker.transform.SetParent(Timeline.transform);
+            Image newImage = newWindowMarker.AddComponent<Image>();
+
+            newImage.rectTransform.anchorMin = new Vector2(((float) i) / (InputManager.Instance.CurrentInputStringLength) - 0.005f, -0.5f);
+            newImage.rectTransform.anchorMax = new Vector2(((float) i) / (InputManager.Instance.CurrentInputStringLength) + 0.005f, 1.5f);
+            newImage.rectTransform.offsetMin = new Vector2(0, 0);
+            newImage.rectTransform.offsetMax = new Vector2(0, 0);
+            newImage.color = Color.red;
+
+            _windowMarkers.Add(newImage);
+        }
+        _windowMarkers[0].color = Color.white;
+
+        GameObject progressBarGO = new GameObject("Progress Bar");
+        progressBarGO.transform.SetParent(Timeline.transform);
+        progressBarGO.transform.localScale = Vector3.one;
+        _progressBar = progressBarGO.AddComponent<Image>();
+
+        _progressBar.rectTransform.anchorMin = new Vector2(0, .1f);
+        _progressBar.rectTransform.anchorMax = new Vector2(0, .9f);
+        _progressBar.rectTransform.offsetMin = new Vector2(0, 0);
+        _progressBar.rectTransform.offsetMax = new Vector2(0, 0);
+        _progressBar.color = Color.white;
+    }
+    void OnInputWindowStart() {
+
+    }
+    void OnInputWindowEnd() {
+        _windowMarkers[InputManager.Instance.InputString.Count].color = Color.white;
+    }
+    void OnInputSeriesEnd() {
+
+    }
+}
