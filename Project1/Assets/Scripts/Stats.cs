b88c6a7dca53989d15f457e893ede198a6a3a313
rowlandsc <cr549@drexel.edu>
2016-01-22 21:29:25 -0500

    pull minor edits

--- Project1/Assets/Scripts/Stats.cs
+++ Project1/Assets/Scripts/Stats.cs
@@ -4,18 +4,18 @@ using System.Collections;
 public class Stats : MonoBehaviour
 {
     
-    public float bAttackMin = 1.0f;
-    public float bAttackMax = 2.0f;
-    public float bHealth = 10.0f;
+    public float bAttackMin = 1.0f; //base
+    public float bAttackMax = 2.0f; //base
+    public float bHealth = 10.0f; //base
 
     public float levelDifMod = 1.0f;
     public int bXpReward = 5;
     public int maxAttacks = 5; //Talk about how we want to scale max attacks
-    public int cLevel = 1;
+    public int cLevel = 1; //Current level
 
-    private float _cHealth;
-    private float _cAttack;
-    private float _cXpReward;
+    private float _cHealth; //Current
+    private float _cAttack; //Current
+    private float _cXp; //Current
 
     
     public CombatSystem.AttackType[] possibleAttacks;
@@ -47,7 +47,7 @@ public class Stats : MonoBehaviour
         //All linear scaling based on dif modifier. Change to logrithmic scaling at some point possibly
         _cHealth = bHealth + (cLevel * levelDifMod * bHealth);
         _cAttack = bAttackMin + (cLevel * levelDifMod * (Random.Range(bAttackMin * 100, bAttackMax * 100) * .01f));
-        _cXpReward = bXpReward + (cLevel * levelDifMod * (Random.Range((bXpReward - 1) * 100, (bXpReward + 1) * 100) * .01f));
+        _cXp = bXpReward + (cLevel * levelDifMod * (Random.Range((bXpReward - 1) * 100, (bXpReward + 1) * 100) * .01f));
     }
    
 }
