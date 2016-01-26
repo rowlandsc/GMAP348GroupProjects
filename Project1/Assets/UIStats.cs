29f53ae778a5e6789018d03d05c1b6339518bde9
Alex Bafaloukos <alexbafaloukos@gmail.com>
2016-01-26 03:39:46 -0500

    More enemy prefabs

--- Project1/Assets/UIStats.cs
+++ Project1/Assets/UIStats.cs
@@ -6,30 +6,30 @@ public class UIStats : MonoBehaviour {
     public Text playerLevelText;
     public Text playerXPText;
     public Text enemyLevelText;
-    public Slider playerHealth;
-    public Slider enemyHealth;
+    public Text playerHealth;
+    public Text enemyHealth;
 	// Use this for initialization
 	void Start ()
     {
 	
 	}
     
-    void onGUI()
-    {
-
-    }
 
 	// Update is called once per frame
 	void Update ()
     {
-        playerLevelText.text = "Level : " + Player.Instance.GetComponent<Stats>().cLevel;
-        playerXPText.text = "Total XP : " + Player.Instance.xpEarned;
+        playerLevelText.text = "Level: " + Player.Instance.GetComponent<Stats>().cLevel;
+        playerXPText.text = "Total XP: " + Player.Instance.xpEarned;
+        playerHealth.text = "Health: " + Player.Instance.GetComponent<Stats>().GetHealth();
         if (CombatSystem.Instance.CurrentEnemy) {
             enemyLevelText.enabled = true;
+            enemyHealth.enabled = true;
             enemyLevelText.text = "Level: " + CombatSystem.Instance.CurrentEnemy.GetComponent<Stats>().cLevel;
+            enemyHealth.text = "Health: " + CombatSystem.Instance.CurrentEnemy.GetComponent<Stats>().GetHealth();
         }
         else {
             enemyLevelText.enabled = false;
+            enemyHealth.enabled = false;
         }
 	}
 }
