2c7b3eec0ee99ac6c92fbc4ec7172e30e5bceff1
Alex Bafaloukos <alexbafaloukos@gmail.com>
2016-01-26 04:06:26 -0500

    Necro prefabs

---
+++ Project1/Assets/Scripts/Necromancer.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+public class Necromancer : Enemy {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    
+	}
+
+    public override List<CombatSystem.AttackType> GetInputSeries(int length) {
+        int maxattacks = GetComponent<Stats>().maxAttacks;
+        List<CombatSystem.AttackType> attackList = new List<CombatSystem.AttackType>();
+        for (int i = 0; i < maxattacks; i++) {
+            if (i == 0 || attackList[i-1] != CombatSystem.AttackType.CHARGE) {
+                if (Random.Range(0, 100) > 50) {
+                    attackList.Add(CombatSystem.AttackType.QUICKATTACK);
+                }
+                else {
+                    attackList.Add(CombatSystem.AttackType.CHARGE);
+                }
+            }
+            else {
+                if (Random.Range(0, 100) > 50) {
+                    attackList.Add(CombatSystem.AttackType.CHARGE);
+                }
+                else {
+                    attackList.Add(CombatSystem.AttackType.SPECIALATTACK);
+                }
+            }
+        }
+        return attackList;
+    }
+}
