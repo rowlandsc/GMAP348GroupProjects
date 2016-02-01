bfd19ff762afd5eaee07e638e798c044032c7c63
Alex Bafaloukos <alexbafaloukos@gmail.com>
2016-02-01 16:46:57 -0500

    Map is drawn

---
+++ Project 2/Assets/Scripts/GameManager.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameManager : MonoBehaviour {
+
+    public Map GameMap;
+    public int GridWidth = 45;
+    public int GridHeight = 20;
+
+	void Start () {
+        GameMap.Initialize(GridWidth, GridHeight);
+	}
+	
+	void Update () {
+	
+	}
+}
