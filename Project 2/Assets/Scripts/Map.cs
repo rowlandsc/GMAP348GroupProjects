53e1672dabebd062a238156d1a340bd0909818fb
Alex Bafaloukos <alexbafaloukos@gmail.com>
2016-02-02 00:19:25 -0500

    Changed how map works

--- Project 2/Assets/Scripts/Map.cs
+++ Project 2/Assets/Scripts/Map.cs
@@ -3,63 +3,43 @@ using System.Collections.Generic;
 using UnityEngine.UI;
 
 public class Map : MonoBehaviour {
-    public class Tile {
-        public int X;
-        public int Y;
 
-        public bool IsTraversable = true;
-        public bool HasBomb = false;
-        public bool IsGoal = false;
-    }
-
-    public List<List<Tile>> TileMap;
+    public List<List<MapTile>> TileMap;
     public Canvas MapCanvas;
+    public float TileWidth = 64;
+    public float TileHeight = 64;
+    public Sprite TileSprite;
 
     public void Initialize(int width, int height) {
-        TileMap = new List<List<Tile>>();
+        TileMap = new List<List<MapTile>>();
         for (int i=0; i< width; i++) {
-            TileMap.Add(new List<Tile>());
+            TileMap.Add(new List<MapTile>());
             for (int j=0; j< height; j++) {
-                TileMap[i].Add(new Tile());
-                TileMap[i][j].X = i;
-                TileMap[i][j].Y = j;
+                GameObject newMapSquare = new GameObject("Map tile " + i + " " + j);
+                newMapSquare.transform.SetParent(transform);
+                newMapSquare.transform.localPosition = new Vector3(i * TileWidth, j * TileHeight, 0);
+                newMapSquare.transform.localScale = new Vector3(TileWidth / TileSprite.bounds.size.x, TileHeight / TileSprite.bounds.size.y, 1);
+                SpriteRenderer sprite = newMapSquare.AddComponent<SpriteRenderer>();
+                sprite.color = Color.white;
+                sprite.sprite = TileSprite;
+                
+
+                MapTile newTile = newMapSquare.AddComponent<MapTile>();
+                TileMap[i].Add(newTile);
+                newTile.X = i;
+                newTile.Y = j;
+                newTile.ParentMap = this;
             }
         }
 
-        for (int i=0; i<=height; i++) {
-            GameObject newGridLineGO = new GameObject("Grid Line Horizontal " + i);
-            RectTransform rt = newGridLineGO.AddComponent<RectTransform>();
-            rt.SetParent(MapCanvas.transform);
-            rt.anchorMin = new Vector2(0, (float)i / height);
-            rt.anchorMax = new Vector2(1, (float)i / height);
-            rt.offsetMin = new Vector2(0, rt.offsetMin.y);
-            rt.offsetMax = new Vector2(0, rt.offsetMax.y);
-            rt.anchoredPosition = new Vector2(0, 0);
-            rt.sizeDelta = new Vector2(rt.sizeDelta.x, 3);
-            rt.localScale = Vector3.one;
+        for (int i=0; i<=width; i++) {
+            for (int j = 0; j <= height; j++) {
                 
-            Image newLine = newGridLineGO.AddComponent<Image>();
-            newLine.color = Color.black;
-        }
-
-        for (int i = 0; i <= width; i++) {
-            GameObject newGridLineGO = new GameObject("Grid Line Vertical " + i);
-            RectTransform rt = newGridLineGO.AddComponent<RectTransform>();
-            rt.SetParent(MapCanvas.transform);
-            rt.anchorMin = new Vector2((float)i / width, 0);
-            rt.anchorMax = new Vector2((float)i / width, 1);
-            rt.offsetMin = new Vector2(rt.offsetMin.x, 0);
-            rt.offsetMax = new Vector2(rt.offsetMax.x, 0);
-            rt.anchoredPosition = new Vector2(0, 0);
-            rt.sizeDelta = new Vector2(3, rt.sizeDelta.y);
-            rt.localScale = Vector3.one;
-
-            Image newLine = newGridLineGO.AddComponent<Image>();
-            newLine.color = Color.black;
+            }
         }
     }
 
-    public Tile GetTile(int x, int y) {
+    public MapTile GetTile(int x, int y) {
         return TileMap[x][y];
     }
 }
---
+++ Project 2/Assets/Scripts/MapTile.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+public class MapTile : MonoBehaviour {
+
+    public int X;
+    public int Y;
+    public Map ParentMap;
+
+    public bool IsTraversable = true;
+    public bool HasBomb = false;
+    public bool IsGoal = false;
+
+    void Start() {
+
+    }
+
+    void Update() {
+
+    }
+}
