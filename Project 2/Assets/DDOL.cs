c22183727134d3c9926b0d5eb20fe3cdb5995a67
rowlandsc <cr549@drexel.edu>
2016-02-09 01:46:23 -0500

    Finished game

---
+++ Project 2/Assets/DDOL.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DDOL : MonoBehaviour
+{
+    public static DDOL Instance = null;
+	// Use this for initialization
+	void Awake ()
+    {
+        if (Instance == null)
+        {
+            DontDestroyOnLoad(this.gameObject);
+            Instance = this;
+        }
+        else
+            Destroy(this.gameObject);
+        
+	}
+	
+}
