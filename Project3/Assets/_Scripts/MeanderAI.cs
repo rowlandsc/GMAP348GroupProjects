a68548b465f329e27533c60453c7d5ecac1ca716
paperclip007 <alexbafaloukos@gmail.com>
2016-02-23 03:34:21 -0500

    Level whatever

--- Project3/Assets/_Scripts/MeanderAI.cs
+++ Project3/Assets/_Scripts/MeanderAI.cs
@@ -6,7 +6,7 @@ public class MeanderAI : EnemyAI {
     protected override void Idle() {
         if (_enemy.CurrentFacing == Character.Facing.RIGHT) {
             transform.position += new Vector3(IdleSpeed * Time.deltaTime, 0, 0);
-            Collider2D col = Physics2D.OverlapArea(_enemy.WeaponCollider.bounds.min, _enemy.WeaponCollider.bounds.max, LayerMask.GetMask("Ground"));
+            Collider2D col = Physics2D.OverlapArea(_enemy.WeaponCollider.bounds.min, _enemy.WeaponCollider.bounds.max, LayerMask.GetMask("Ground", "EnemyInvisibleWall"));
             if (col != null) {
                 transform.localScale = new Vector3(-1 * transform.localScale.x, transform.localScale.y, transform.localScale.z);
             }
