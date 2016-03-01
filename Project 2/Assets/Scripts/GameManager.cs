479844e6ad8affcbcd3edda2774fff7a89cbd6ba
Alex Bafaloukos <alexbafaloukos@gmail.com>
2016-03-01 04:08:10 -0500

    Hotline Miami this jawn

--- Project 2/Assets/Scripts/GameManager.cs
+++ Project 2/Assets/Scripts/GameManager.cs
@@ -28,6 +28,8 @@ public class GameManager : MonoBehaviour {
     public GameObject Player2Wins;
     public GameObject Tie;
 
+    public float TileRevealInterval = 0.1f;
+
     void Start () {
         GameMap.Initialize(GridWidth, GridHeight, PercentBombs, NumberOfPlayers);
         p1.gameObject.SetActive(true);
@@ -47,7 +49,26 @@ public class GameManager : MonoBehaviour {
         PlayerMovement player1 = p1.GetComponent<PlayerMovement>();
         PlayerMovement player2 = p2.GetComponent<PlayerMovement>();
 
-        for (int i=0; i<Map.Instance.TileMap.Count; i++) {
+        StartCoroutine(RevealMap());
+
+        GameOverScreen.SetActive(true);
+        if (player1.Score > player2.Score) {
+            Player1Wins.SetActive(true);
+        }
+        else if (player1.Score < player2.Score) {
+            Player2Wins.SetActive(true);
+        }
+        else {
+            Tie.SetActive(true);
+        }
+    }
+
+    IEnumerator RevealMap() {
+
+        PlayerMovement player1 = p1.GetComponent<PlayerMovement>();
+        PlayerMovement player2 = p2.GetComponent<PlayerMovement>();
+
+        /*for (int i = 0; i < Map.Instance.TileMap.Count; i++) {
             for (int j = 0; j < Map.Instance.TileMap[i].Count; j++) {
                 MapTile tile = Map.Instance.TileMap[i][j];
                 if (tile.HasBomb) {
@@ -68,18 +89,54 @@ public class GameManager : MonoBehaviour {
                         player2.Score += ScoreManager.Instance.SCORE_INCORRECT_MARK;
                     }
                 }
+                yield return new WaitForSeconds(TileRevealInterval);
             }
-        }
+        }*/
 
-        GameOverScreen.SetActive(true);
-        if (player1.Score > player2.Score) {
-            Player1Wins.SetActive(true);
-        }
-        else if (player1.Score < player2.Score) {
-            Player2Wins.SetActive(true);
-        }
-        else {
-            Tie.SetActive(true);
+        for (int i = Map.Instance.TileMap.Count - 1; i >= 0;  i--) {
+            for (int j = 0; j < Map.Instance.TileMap[i].Count / 2.0f; j++) {
+                MapTile tileDown = Map.Instance.TileMap[i][Mathf.FloorToInt(Map.Instance.TileMap[i].Count / 2.0f) - j - 1];
+                if (tileDown.HasBomb) {
+                    tileDown.IsVisible = true;
+                    if (tileDown.P1Marked) {
+                        player1.Score += ScoreManager.Instance.SCORE_CORRECT_MARK;
+                    }
+                    if (tileDown.P2Marked) {
+                        player2.Score += ScoreManager.Instance.SCORE_CORRECT_MARK;
+                    }
+                }
+                else {
+                    tileDown.IsExplored = true;
+                    if (tileDown.P1Marked) {
+                        player1.Score += ScoreManager.Instance.SCORE_INCORRECT_MARK;
+                    }
+                    if (tileDown.P2Marked) {
+                        player2.Score += ScoreManager.Instance.SCORE_INCORRECT_MARK;
+                    }
+                }
+
+                MapTile tileUp = Map.Instance.TileMap[i][Mathf.FloorToInt(Map.Instance.TileMap[i].Count / 2.0f) + j];
+                if (tileUp.HasBomb) {
+                    tileUp.IsVisible = true;
+                    if (tileUp.P1Marked) {
+                        player1.Score += ScoreManager.Instance.SCORE_CORRECT_MARK;
+                    }
+                    if (tileUp.P2Marked) {
+                        player2.Score += ScoreManager.Instance.SCORE_CORRECT_MARK;
+                    }
+                }
+                else {
+                    tileUp.IsExplored = true;
+                    if (tileUp.P1Marked) {
+                        player1.Score += ScoreManager.Instance.SCORE_INCORRECT_MARK;
+                    }
+                    if (tileUp.P2Marked) {
+                        player2.Score += ScoreManager.Instance.SCORE_INCORRECT_MARK;
+                    }
+                }
+
+                yield return new WaitForSeconds(TileRevealInterval);
+            } 
         }
     }
 }
---
+++ Project 2/Assets/Scripts/GameOverPulsate.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+using DG.Tweening;
+
+public class GameOverPulsate : MonoBehaviour {
+
+    public float Variance = 0.2f;
+    public float RotateAngle = 15;
+    public float RotateTime = 2.0f;
+
+	void OnEnable() {
+        StartCoroutine(RotationCoroutine());
+
+    }
+
+    IEnumerator RotationCoroutine() {
+
+        yield return transform.DORotate(new Vector3(0, 0, -1 * RotateAngle * (1 + Random.Range(-1 * Variance, Variance))), RotateTime / 2 * (1 + Random.Range(-1 * Variance, Variance))).WaitForCompletion();
+        while (enabled) {
+            yield return transform.DORotate(new Vector3(0, 0, RotateAngle * (1 + Random.Range(-1 * Variance, Variance))), RotateTime * (1 + Random.Range(-1 * Variance, Variance))).WaitForCompletion();
+            yield return transform.DORotate(new Vector3(0, 0, -1 * RotateAngle * (1 + Random.Range(-1 * Variance, Variance))), RotateTime * (1 + Random.Range(-1 * Variance, Variance))).WaitForCompletion();
+        }
+    }
+}
